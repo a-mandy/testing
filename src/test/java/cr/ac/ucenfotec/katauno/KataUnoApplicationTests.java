6867a397fac4dd69dcb8f1520e927ab03daaf631
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2018-02-11 16:31:33 -0600

    hola mundo

---
+++ src/test/java/cr/ac/ucenfotec/katauno/KataUnoApplicationTests.java
@@ -0,0 +1,16 @@
+package cr.ac.ucenfotec.katauno;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KataUnoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
